984eab43716c11fc4114dee9cd77ca15b67bf607
gotwilk890 <dave.wilk890@gmail.com>
2015-11-09 14:33:48 -0500

    Adds object which will be used to help communicate with back end.
    -Contains host URL
    -Contains ajaz request function with .done and .fail.
      Helps with sending and recieving the data from the back end.

---
+++ front-end-proj-2/authAPI.js
@@ -0,0 +1,16 @@
+"use strict"
+$(document).ready(function(){
+  var authAPI = {
+
+    api_url: 'http://localhost:3000',
+
+    ajax: function(config, cb){
+      $ajax(config).done(function(data, textStatus, jqhxr){
+        cb(null, data);
+      }).fail(function(jqhxr, status, error){
+        cb({jqhxr: jqhxr, statur: status, error: error});
+        });
+      }
+  };
+});
+
