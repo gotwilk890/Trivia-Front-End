7e50470cc02a0ff1b643ba9de1625a5fc2221250
gotwilk890 <dave.wilk890@gmail.com>
2015-11-09 15:13:18 -0500

    Moves Document.ready, added multiplefunctions to help process form data.
    Moves DOM loading to not requirre the api object. Does not need to be loaded.
    
    Adds form2Object method to help convert the HTML form data into an object.
    
    Adds wrap method to take form data and convert it into the proper json for ajax methods.
    
    Adds generic callback method to help process data whether its a successful call or failed call.

--- front-end-proj-2/authAPI.js
+++ front-end-proj-2/authAPI.js
@@ -1,29 +1,59 @@
 "use strict"
+
+var authAPI = {
+
+  api_url: 'http://localhost:3000',
+
+  ajax: function(config, cb){
+    $ajax(config).done(function(data, textStatus, jqhxr){
+      cb(null, data);
+    }).fail(function(jqhxr, status, error){
+      cb({jqhxr: jqhxr, statur: status, error: error});
+      });
+    },
+
+  register: function(credentials, callback){
+    this.ajax({
+      method: 'POST',
+      url: this.api_url +'/users',
+      contentType:'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+
+
+
+  }
+};
+
 $(document).ready(function(){
-  var authAPI = {
 
-    api_url: 'http://localhost:3000',
-
-    ajax: function(config, cb){
-      $ajax(config).done(function(data, textStatus, jqhxr){
-        cb(null, data);
-      }).fail(function(jqhxr, status, error){
-        cb({jqhxr: jqhxr, statur: status, error: error});
-        });
-      },
-
-    register: function(credentials, callback){
-      this.ajax({
-        method: 'POST',
-        url: this.api_url +'/users',
-        contentType:'application/json; charset=utf-8',
-        data: JSON.stringify(credentials),
-        dataType: 'json'
-      }, callback);
-
-
-
-    }
+  var form2object = function(form) {
+    var data = {};
+    $(form).children().each(function(index, element) {
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
+    });
+    return data;
   };
-});
 
+  var wrap = function wrap(root, formData) {
+    var wrapper = {};
+    wrapper[root] = formData;
+    return wrapper;
+  };
+
+  var callback = function callback(error, data) {
+    if (error) {
+      console.error(error);
+      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      return;
+    }
+    $('#result').val(JSON.stringify(data, null, 4));
+  };
+
+
+
+});
