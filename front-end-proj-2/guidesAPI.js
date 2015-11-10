b1b652c33c5cbb9e1f7fdd2bca29fc2b7252a4a0
gotwilk890 <dave.wilk890@gmail.com>
2015-11-10 14:49:56 -0500

    Adds Ajax function
    This will be used by functions to create ajax requests and then interpret the
    data coming back.

--- front-end-proj-2/guidesAPI.js
+++ front-end-proj-2/guidesAPI.js
@@ -4,4 +4,12 @@ var guidesAPI = {
 
   api_url: 'http://localhost:3000',
 
-}
+  ajax: function(config, cb){
+    $.ajax(config).done(function(data, textStatus, jqhxr){
+      cb(null, data);
+    }).fail(function(jqhxr, status, error){
+      cb({jqhxr: jqhxr, statur: status, error: error});
+      });
+    },
+
+};
