1ae045f9d2c7d97dd01f792140d9236ecdf91aeb
gotwilk890 <dave.wilk890@gmail.com>
2015-11-11 14:40:13 -0500

    Adds Ajax request and event handler
    Adds create to the object and is used in the click form click handler
    This was tested in the browser and successfully created a guide in the database.

--- front-end-proj-2/guidesAPI.js
+++ front-end-proj-2/guidesAPI.js
@@ -22,6 +22,19 @@ var guidesAPI = {
       contentType:'application/json; charset=utf-8',
       dataType: 'json'
     }, callback);
+  },
+
+  create: function(guide, token, callback){
+    this.ajax({
+      method: 'POST',
+      url: this.api_url +'/guides',
+      headers: {
+      Authorization: 'Token token=' + token
+      },
+      contentType:'application/json; charset=utf-8',
+      data: JSON.stringify(guide),
+      dataType: 'json'
+    }, callback);
   }
 
 };
@@ -38,6 +51,20 @@ $(document).ready(function(){
       callback(null, data);
       };
     guidesAPI.showGuides(token, cb);
+    e.preventDefault();
+  });
+
+  $('#createGuide').on('submit', function(e){
+    var token = $('.token').val();
+    var guide = wrap('guide', form2object(this));
+    var cb = function cb(error, data) {
+      if (error) {
+        callback(error);
+        return;
+      }
+    callback(null, data);
+    };
+    guidesAPI.create(guide, token, cb);
     e.preventDefault();
   });
 
